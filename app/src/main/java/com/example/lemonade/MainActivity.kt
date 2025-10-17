f074fe3d06cbb0856430e38b4cf15f7edeadf4b4
benvarzam <benje1612@gmail.com>
2025-10-17 19:37:46 +0200

    Practica terminada

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -32,7 +32,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
 import kotlin.random.Random
-
+//Lemonade
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
